0ad556226d273385607c32a8a85c349ee6fb6cc3
chawit033 <61050033@kmitl.ac.th>
2021-07-04 20:43:06 +0700

    can pull data

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import bandList from './bands.json'
+
+import './main.scss'
+
+document.addEventListener('DOMContentLoaded', function() {
+    const cards = document.getElementById('card')
+    const bands = bandList.data
+
+    console.log(bands)
+
+    insertBand(bands,cards)
+
+})
+
+function insertBand(bands,cards) {
+    bands.forEach(band => {
+        let cardDOM = builtCardDOM(band)
+        cards.insertAdjacentHTML('beforeend', cardDOM)
+    })
+
+}
+        
+
+function builtCardDOM(band) {
+    let img = bandList.image_base_url
+    return (
+        `<div class="card">
+            <img src="${img.replace(`xxxARTIST-IDxxx`, `${band.art_id}`)}" alt="#" />
+            <div class="album">${band.album_title}<div>
+            <div class="artist">${band.artist_title}<div>
+            <div class="genre">${band.genre_text}</div>
+        </div>`
+
+    )
+}
